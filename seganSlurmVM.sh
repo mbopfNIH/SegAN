034bac2e64f237ae0b03f57551c9f3cdd9b1a7f6
mbopfNIH <mbopf@nih.gov>
2018-07-10 09:56:46 -0400

    Created seganSlurmVM.sh with batchsize 4 to fit on the VM and upped batchsize to on seganSlurmDGX to maximize memory utilization.

--- seganSlurmVM.sh
+++ seganSlurmVM.sh
@@ -3,9 +3,9 @@
 #SBATCH --workdir=/slurm_storage/public/AMIT/projects/SegAN
 #SBATCH --output=slurmOut/slurm_%j.out
 #SBATCH --error=slurmOut/slurm_%j.error
-#SBATCH --job-name=SegAN_dgx
+#SBATCH --job-name=SegAN_VM
 #SBATCH --gres=gpu:1
-#SBATCH --partition=dgx1
+#SBATCH --partition=VM 
 
 export PATH=/slurm_storage/public/cuda8.0/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/slurm_storage/public/cuda8/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
@@ -16,4 +16,4 @@ mkdir outputs_$SLURM_JOB_ID
 env | sort
 nvidia-smi
 nvcc --version
-python train.py --cuda --batchSize 48 --niter 200 --outpath ./outputs_$SLURM_JOB_ID
+python train.py --cuda --batchSize 4 --niter 200 --outpath ./outputs_$SLURM_JOB_ID
