f72e44ee111726ece0c49248def01113b6fd70f6
mbopfNIH <mbopf@nih.gov>
2018-08-01 12:34:24 -0400

    Added display parameter

--- maskSegan.py
+++ maskSegan.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as plt
 #
 # 17 July 2018 - Mike Bopf
 ############################################################################################
-def maskSegan(input, label, result, color, labelmaskfile, resultmaskfile):
+def maskSegan(input, label, result, color, labelmaskfile, resultmaskfile, display):
     if not os.path.isfile(input):
         print("Error: Input file does not exist: ", input)
         return
@@ -56,6 +56,9 @@ def maskSegan(input, label, result, color, labelmaskfile, resultmaskfile):
     result_mask.putalpha(mask)
     input_img.paste(result_mask, (0,0), result_mask)
     input_img.save(resultmaskfile)
+    if display:
+        result_mask.show()
+        input_img.show()
 
 # Use argparse to take in command line arguments
 parser = argparse.ArgumentParser(description='Example')
@@ -65,6 +68,7 @@ parser.add_argument('--result', default='result_val.png', help='label mask image
 parser.add_argument('--color', default='(0,0,255,200)', help='color of overlaid mask')
 parser.add_argument('--labelmask', default='label_mask.png', help='masked label output file name')
 parser.add_argument('--resultmask', default='result_mask.png', help='masked result output file name')
+parser.add_argument('--display', default=False, help='Should masks attempt to be displayed')
 opts = parser.parse_args()
 
-maskSegan(opts.input, opts.label, opts.result, opts.color, opts.labelmask, opts.resultmask)
+maskSegan(opts.input, opts.label, opts.result, opts.color, opts.labelmask, opts.resultmask, opts.display)
